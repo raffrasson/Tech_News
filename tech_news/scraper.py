aa32c9537206685aeecb14d1c8ab4ff28fe92396
raffrasson <raf.frasson@gmail.com>
2022-10-06 22:32:36 -0300

    completes req. 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 def fetch(url):
@@ -17,7 +18,9 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    link_list = selector.css("h2 a::attr(href)").getall()
+    return link_list
 
 
 # Requisito 3
