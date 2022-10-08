874a7b716dd7c29afaef2b80a07365cf64371632
raffrasson <raf.frasson@gmail.com>
2022-10-08 19:57:50 -0300

    completes req. 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -24,13 +24,34 @@ def scrape_novidades(html_content):
 
 def scrape_next_page_link(html_content):
     selector = Selector(text=html_content)
-    link_list = selector.css("a.next.page-numbers::attr(href)").get()
-    return link_list
+    next = selector.css("a.next.page-numbers::attr(href)").get()
+    return next
 
 
-# Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    news_dict = {
+        "url": selector.css("link[rel=canonical]::attr(href)").get(),
+        "title": selector.css("h1[class=entry-title]::text").get().strip(),
+        "timestamp": selector.css("li[class=meta-date]::text").get(),
+        "writer": selector.css("a.url.fn.n::text").get(),
+        "comments_count": len(selector.css("ol.comment-list.li").getall()),
+        "summary": "".join(
+            selector.css(".entry-content > p:nth-of-type(1) *::text").getall()
+        ).strip(),
+        # refs:
+        # https://www.simplilearn.com/
+        # tutorials/python-tutorial/list-to-string-in-python e o método strip
+        # encontrado na lista:
+        # https://www.w3schools.com/python/python_strings_methods.asp
+        "tags": selector.css("a[rel=tag]::text").getall(),
+        "category": selector.css(
+            ".entry-details .meta-category .category-style span.label::text"
+        ).get(),
+    }
+
+    print(news_dict)
+    return news_dict
 
 
 # Requisito 5
