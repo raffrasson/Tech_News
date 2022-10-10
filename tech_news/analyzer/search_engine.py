0a86875e25644287b1470ef6e9169aa7c8c4972e
raffrasson <raf.frasson@gmail.com>
2022-10-10 12:01:10 -0300

    completes req. 6 and 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import datetime
 
 
 def search_by_title(title):
@@ -12,7 +13,23 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    # referencias:
+    # https://theprogrammingexpert.com/python-remove-time-from-datetime/
+    # https://stackoverflow.com/questions/969285/how-do-i-translate-an-iso-8601-datetime-string-into-a-python-datetime-object
+    try:
+        converted_date = datetime.datetime.fromisoformat(date).strftime(
+            "%d/%m/%Y"
+        )
+        tupled_results = []
+        results = search_news(
+            {"timestamp": {"$regex": converted_date, "$options": "i"}}
+        )
+        for result in results:
+            tupled_results.append((result["title"], result["url"]))
+        return tupled_results
+
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
